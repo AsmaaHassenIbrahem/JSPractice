c9269ae7dc29ef825766bef984c75a015bb162bd
Asma <asmaa.shabaan@vodafone.com>
2023-11-19 13:50:03 +0200

    update without es6

--- oopWithoutES6/TestWithoutES6.js
+++ oopWithoutES6/TestWithoutES6.js
@@ -13,63 +13,63 @@
 // box2 should return 7
 
 function Book(title) {
-    this.title = title;
+  this.title = title;
+}
 
-    Book.totalBooks = (Book.totalBooks || 0) + 1;
-  }
-  
-  function Box() {
-    this.content = [];
+function Box() {
+  this.content = [];
 
-    this.toString = function () {
-      return `Box dimensions: ${this.content.length} books`;
-    };
-  }
-  
-  Box.prototype.addBook = function (book) {
+  this.toString = function () {
+    return `Box dimensions: ${this.content.length} books`;
+  };
+}
+
+Box.prototype.addBook = function (book) {
+      Box.totalBooks = (Box.totalBooks || 0) + 1;
+
     this.content.push(book);
-  };
-  
-  Box.prototype.countBooks = function () {
-    return this.content.length;
-  };
-  
-  Box.prototype.deleteBook = function (title) {
-    const index = this.content.findIndex(book => book.title === title);
-    if (index !== -1) {
-      this.content.splice(index, 1);
-    }
-  };
-  
-  Box.totalBooks = 0;
-  Box.getTotalBooks = function () {
-    return Box.totalBooks;
-  };
-  
-  Box.prototype.valueOf = function () {
-    return this.content.length;
-  };
-  
-  // Example usage:
-  const box1 = new Box();
-  const book1 = new Book("The Secret");
-  const book2 = new Book("To Kill a Mockingbird");
-  
-  box1.addBook(book1);
-  box1.addBook(book2);
-  
-  console.log(box1.toString()); // Output: Box dimensions: 2 books
-  
-  const box2 = new Box();
-  const book3 = new Book("The Secret");
-  
-  box2.addBook(book3);
-  
-  console.log(box1 + box2); // Output: 3 (total number of books in both boxes)
-  
-  box1.deleteBook("The Secret");
-  console.log(box1.toString()); // Output: Box dimensions: 1 book
-  
-  console.log(Box.getTotalBooks()); // Output: 3 (total number of created books)
-  
-  
+};
+
+Box.prototype.countBooks = function () {
+  return this.content.length;
+};
+
+Box.prototype.deleteBook = function (title) {
+  const index = this.content.findIndex(book => book.title === title);
+  if (index !== -1) {
+    this.content.splice(index, 1);
+    Box.totalBooks = (Box.totalBooks || 0) - 1;
+
+  }
+};
+
+Box.totalBooks = 0;
+Box.getTotalBooks = function () {
+  return Box.totalBooks;
+};
+
+Box.prototype.valueOf = function () {
+  return this.content.length;
+};
+
+// Example usage:
+const box1 = new Box();
+const book1 = new Book("The Secret");
+const book2 = new Book("To Kill a Mockingbird");
+
+box1.addBook(book1);
+box1.addBook(book2);
+
+console.log(box1.toString()); // Output: Box dimensions: 2 books
+
+const box2 = new Box();
+const book3 = new Book("The Secret");
+
+box2.addBook(book3);
+
+console.log(box1 + box2); // Output: 3 (total number of books in both boxes)
+
+box1.deleteBook("The Secret");
+console.log(box1.toString()); // Output: Box dimensions: 1 book
+
+console.log(Box.getTotalBooks()); // Output: 3 (total number of created books)
